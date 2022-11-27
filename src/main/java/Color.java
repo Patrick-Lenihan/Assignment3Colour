39febadd6bee5303634da13a7f02a83c448d9897
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 16:11:11 +0000

    refactor the Color class with javadoc comments.
    this should have been done after each time I made a test case pas but I forgot to do it at the time.

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,16 +1,70 @@
+/**
+ * @author patrick-lenihan
+ *
+ * this is a class that represents color in rgb format
+ *
+ */
 public class Color {
+    /**
+     * the amount of red in the color this
+     * class represents
+     */
     private int amountRed;
+    /**
+     * the amount of green in the color this
+     * class represents
+     */
     private int amountGreen;
 
+    /**
+     * the amount of blue in the color this
+     * class represents.
+     */
+    private int amountBlue;
+
+    /**
+     * this is a contructor for Color
+     * @param amountRed the amount of red in the color
+     *                  this class represents.
+     * @param amountGreen the amount of green in the color
+     *                  this class represents.
+     * @param amountBlue the amount of red in the color
+     *                  this class represents.
+     */
     public Color(float amountRed, float amountGreen, float amountBlue){
         this.amountRed = (int) amountRed*255;
         this.amountGreen = (int) (amountGreen*255);
-        System.out.println(this.amountGreen);
+        this.amountBlue = (int) (amountBlue*255);
     }
+
+    /**
+     * the getter for amountRed
+     *
+     * @return the amount of red
+     *          in the color
+     *          this class represents
+     */
     public int getRed(){
         return amountRed;
     }
+
+    /**
+     * the getter for amountGreen
+     *
+     * @return the amount of green
+     * in the color this class represents
+     */
     public int getGreen(){
         return amountGreen;
+    }
+
+    /**
+     * the getter for amountBlue
+     *
+     * @return the amount of Blue
+     * in the color this class represents
+     */
+    public int getBlue(){
+        return amountBlue;
     }
 }
--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -6,6 +6,7 @@ public class testColor {
         Color rgbColorInstance = new Color((float)1.0,(float)0.12549019607,(float) 0.1294117647);
         assertEquals(rgbColorInstance.getRed(),255,"red colors do not match");
         assertEquals(rgbColorInstance.getGreen(),32,"green colors do not match");
+        assertEquals(rgbColorInstance.getBlue(),33,"blue colors do not match");
     }
 
 }
