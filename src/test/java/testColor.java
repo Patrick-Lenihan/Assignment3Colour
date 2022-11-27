ddd60ee982f4310847346d73836b777bd6026610
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 15:37:03 +0000

    Create Failing unit test for the green value in Color class

--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -5,6 +5,7 @@ public class testColor {
     void testColorInit(){
         Color rgbColorInstance = new Color((float)1.0,(float)0.1294117647,(float) 0.1294117647);
         assertEquals(rgbColorInstance.getRed(),255,"red colors do not match");
+        assertEquals(rgbColorInstance.getGreen(),32,"green colors do not match");
     }
 
 }
