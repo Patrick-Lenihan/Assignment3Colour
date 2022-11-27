dff3aa7de9cc29b8af1b231cb6a35ff758e4dd97
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 14:58:44 +0000

    Fix bugs in tests
    declare create rgbColorInstance properly and make sure its inputs are explicitly floats.

--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -1,10 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import Color;
 public class testColor {
     @Test
     void testColorInit(){
-        rgbColorInstance = Color(1.0,0.1294117647,0.1294117647);
+        Color rgbColorInstance = new Color((float)1.0,(float)0.1294117647,(float) 0.1294117647);
         assertEquals(rgbColorInstance.getRed(),255,"red colors do not match");
     }
 
