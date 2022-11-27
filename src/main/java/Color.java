3ce93bda06e32676b87d41230f2b4b33c78ad5d9
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 15:45:54 +0000

    create a getter for the green color value in Color.java
    take float input and return as int output in the range between 0-255

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,10 +1,16 @@
 public class Color {
     private int amountRed;
+    private int amountGreen;
 
     public Color(float amountRed, float amountGreen, float amountBlue){
         this.amountRed = (int) amountRed*255;
+        this.amountGreen = (int) (amountGreen*255);
+        System.out.println(this.amountGreen);
     }
     public int getRed(){
         return amountRed;
+    }
+    public int getGreen(){
+        return amountGreen;
     }
 }
