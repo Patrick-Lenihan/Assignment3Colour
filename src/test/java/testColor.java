553253e8829a52ca7f34381a49788118396ae673
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 20:09:16 +0000

    create a unit test that tests that the color constructor does not allow arguments that are outside the allowed ranges of 0 to 1

--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class testColor {
     @Test
     void testColorInit(){
@@ -7,8 +12,27 @@ public class testColor {
         assertEquals(255,rgbColorInstance.getRed(),"red colors do not match");
         assertEquals(32,rgbColorInstance.getGreen(),"green colors do not match");
         assertEquals(33,rgbColorInstance.getBlue(),"blue colors do not match");
+
         rgbColorInstance = new Color((float)0.21568627451,(float)0.12549019607,(float) 0.1294117647);
         assertEquals(55,rgbColorInstance.getRed(),"red colors do not match: ");
+
+
+    }
+    @ParameterizedTest
+    @ValueSource(floats={(float) 1.1,(float) -0.1})
+    void constructorShouldNotAcceptOutOfRangeValues(float outOfRangeVal){
+
+        Exception checkRedOutOfRange = assertThrows(IllegalArgumentException.class, () ->
+                new Color(outOfRangeVal,(float)0.12549019607,(float) 0.1294117647));
+        assertEquals("value must be between 0 and 1", checkRedOutOfRange.getMessage());
+
+        Exception checkGreenOutOfRange = assertThrows(IllegalArgumentException.class, () ->
+                new Color((float)1.0,outOfRangeVal,(float) 0.1294117647));
+        assertEquals("value must be between 0 and 1", checkGreenOutOfRange.getMessage());
+
+        Exception checkBlueOutOfRange = assertThrows(IllegalArgumentException.class, () ->
+                new Color((float)1.0,(float)0.12549019607,outOfRangeVal));
+        assertEquals("value must be between 0 and 1", checkBlueOutOfRange.getMessage());
     }
 
 }
