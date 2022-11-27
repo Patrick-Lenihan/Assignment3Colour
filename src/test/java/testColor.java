c38c791a14a0ed9047a461441d65682a5df77140
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 16:23:16 +0000

    create a test that passes in a float that represents a number that has non-zero numbers after the decimal point as the red value in Color.
    rearrange the expected and actual values in the assert equals for better error messages.

--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -4,9 +4,11 @@ public class testColor {
     @Test
     void testColorInit(){
         Color rgbColorInstance = new Color((float)1.0,(float)0.12549019607,(float) 0.1294117647);
-        assertEquals(rgbColorInstance.getRed(),255,"red colors do not match");
-        assertEquals(rgbColorInstance.getGreen(),32,"green colors do not match");
-        assertEquals(rgbColorInstance.getBlue(),33,"blue colors do not match");
+        assertEquals(255,rgbColorInstance.getRed(),"red colors do not match");
+        assertEquals(32,rgbColorInstance.getGreen(),"green colors do not match");
+        assertEquals(33,rgbColorInstance.getBlue(),"blue colors do not match");
+        rgbColorInstance = new Color((float)0.21568627451,(float)0.12549019607,(float) 0.1294117647);
+        assertEquals(55,rgbColorInstance.getRed(),"red colors do not match: ");
     }
 
 }
