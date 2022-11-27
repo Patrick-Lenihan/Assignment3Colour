5662bf8ccbcc25cfac98d9068acd9410932292a1
Patrick-Lenihan <pdlenihan@gmail.com>
2022-11-27 15:43:57 +0000

    fix unit test in testColorInit
    the green color unit test was incorrect and was asserting 32 when the input should evaluate to 33.
    
    changed the input to make it 32.

--- src/test/java/testColor.java
+++ src/test/java/testColor.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class testColor {
     @Test
     void testColorInit(){
-        Color rgbColorInstance = new Color((float)1.0,(float)0.1294117647,(float) 0.1294117647);
+        Color rgbColorInstance = new Color((float)1.0,(float)0.12549019607,(float) 0.1294117647);
         assertEquals(rgbColorInstance.getRed(),255,"red colors do not match");
         assertEquals(rgbColorInstance.getGreen(),32,"green colors do not match");
     }
